5832ed3d5e6353858f0e3305a6177de662d3ca69
François Michaud <fmichaud@9fevrier.com>
2018-02-20 23:38:51 +0100

    Initial import

---
+++ app/templates/lib/index.js
@@ -0,0 +1,8 @@
+/**
+ * This file is part of <%= github.repository %>
+ *
+ * Copyright (c) <%= year %> <%= author.name %>.
+ *
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ *
+ */
